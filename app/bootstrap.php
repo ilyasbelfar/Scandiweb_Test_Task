fd5c7f6af37e780adbd8b01c6c6ff0aefa04ce5f
Ilyas Belfar <i.belfar@esi-sba.dz>
2023-04-27 20:49:48 +0000

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -14,4 +14,10 @@ spl_autoload_register(function ($classname) {
     if (file_exists($class_path)) require_once($class_path);
 });
 
+// Autoload controllers
+spl_autoload_register(function ($classname) {
+    $class_path = __DIR__ . '/controllers/' . str_replace('\\', '/', $classname) . '.php';
+    if (file_exists($class_path)) require_once($class_path);
+});
+
 $core = new Core();
